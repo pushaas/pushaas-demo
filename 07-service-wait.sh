637d812cce23de192157668ae5faca62d8f5eddf
rafaeleyng <rafaeleyng@gmail.com>
2019-08-25 03:41:59 -0300

    write wait service

--- 07-service-wait.sh
+++ 07-service-wait.sh
@@ -5,4 +5,9 @@
 SERVICE_INSTANCE_NAME="push-$SUFFIX"
 COMMAND="tsuru service-instance-bind pushaas $SERVICE_INSTANCE_NAME -a $APP_NAME --no-restart"
 
-tsuru service-instance-info pushaas $SERVICE_INSTANCE_NAME
+while true
+do
+  STATUS=$(tsuru service-instance-info pushaas $SERVICE_INSTANCE_NAME | grep "Status: Service instance")
+  echo $STATUS
+  sleep 1
+done
--- 99-teardown.sh
+++ 99-teardown.sh
@@ -7,8 +7,8 @@ APP_NAME="push-service-demo-$SUFFIX"
 SERVICE_INSTANCE_NAME="push-$SUFFIX"
 COMMAND="tsuru service-instance-bind pushaas $SERVICE_INSTANCE_NAME -a $APP_NAME"
 
-tsuru service-instance-unbind pushaas $SERVICE_INSTANCE_NAME -a $APP_NAME --no-restart
-# tsuru service-instance-remove pushaas $SERVICE_INSTANCE_NAME
+# tsuru service-instance-unbind pushaas $SERVICE_INSTANCE_NAME -a $APP_NAME --no-restart
+# tsuru service-instance-remove pushaas $SERVICE_INSTANCE_NAME -y
 
 # tsuru app-remove -a $APP_NAME -y
 # rm -fr push-service-demo-app
