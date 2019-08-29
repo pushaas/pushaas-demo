c45a61cd8d0d34ae29992c93c001c9c687bd9148
rafaeleyng <rafaeleyng@gmail.com>
2019-08-28 23:31:13 -0300

    improve "open-app" logs

--- 04-open-app.sh
+++ 04-open-app.sh
@@ -6,6 +6,9 @@ echo
 . .vars
 APP_NAME="push-service-demo-$SUFFIX"
 APP_PORT=$(tsuru app-info -a $APP_NAME | grep "| start" | awk -F "|" '{print $5}' | xargs)
+APP_ADDRESS=$(tsuru app-info -a $APP_NAME | grep "Address: " | awk -F " " '{print $2}' | xargs)
 
-echo "Check your app: http://$TSURU_POOL_IP:$APP_PORT"
-echo
+echo "Check your app:"
+echo "  - http://$TSURU_POOL_IP:$APP_PORT"
+echo "  or"
+echo "  - http://$APP_ADDRESS"
